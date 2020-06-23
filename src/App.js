0d63b359f56fc7d7d3a0f8d244d9cef1ce93acfe
Alvi Geovanny <Alvigeovan29@gmail.com>
2020-06-23 22:29:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Titles from './component/Titles';
+import Form from './component/Form';
+import Weather from './component/Weather';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = "8b8a2a4a64a0391912ad8bd7b4c1503c";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Titles />
+        <Form />
+        <Weather />
+      </div>
+    )
+  }
 }
 
 export default App;
